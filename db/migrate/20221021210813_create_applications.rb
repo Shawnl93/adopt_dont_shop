39b9b1e7ff3a6234a19579338a5728924f8f6f94
Shawn Lee <Shawnl93@hotmail.com>
2022-10-21 15:10:46 -0600

    set up many_to_many relationship

---
+++ db/migrate/20221021210813_create_applications.rb
@@ -0,0 +1,14 @@
+class CreateApplications < ActiveRecord::Migration[5.2]
+  def change
+    create_table :applications do |t|
+      t.string :name
+      t.string :address
+      t.string :description
+      t.string :application_status
+      t.string :street
+      t.string :city
+      t.string :state
+      t.integer :zip_code
+    end
+  end
+end
