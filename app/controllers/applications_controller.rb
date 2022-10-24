40c758c0ef2c8dcf722283435bfc30cd793d4c0c
Shawn Lee <Shawnl93@hotmail.com>
2022-10-23 20:06:48 -0600

    Made new application page

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -6,4 +6,16 @@ class ApplicationsController < ApplicationController
   def new
 
   end
+
+  def create
+    application = Application.create(app_params)
+    binding.pry
+    redirect_to "/applications/#{application.id}"
+  end
+
+private
+
+  def app_params
+    params.permit(:name, :street, :city, :state, :zip_code, :description)
+  end
 end
--- spec/features/applications/create_spec.rb
+++ spec/features/applications/create_spec.rb
@@ -7,5 +7,23 @@ RSpec.describe "Makes new applications" do
       click_on "Start an Application"
       expect(current_path).to eq('/applications/new')
     end
+
+    it "can make a new applicant" do
+      visit '/applications/new'
+      fill_in :name, with: "Shawn 2.0"
+      fill_in :street, with: "123 the st."
+      fill_in :city, with: "MyCity"
+      fill_in :state, with: "United States"
+      fill_in :zip_code, with: "55555"
+      fill_in :description, with: "im better than Shawn 1.0"
+      click_on "Submit"
+      shawn2 = Application.last.id
+      expect(current_path).to eq("/applications/#{shawn2}")
+      expect(page).to have_content("In Progress")
+    end
+
+    it "" do
+
+    end
   end
 end
