60c5672f74f7bef8f5aecea5515b7153b821e30d
Shawn Lee <Shawnl93@hotmail.com>
2022-10-24 11:13:14 -0600

    US 3 completed and UPDATED SCHEMA to remove address column

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -8,8 +8,13 @@ class ApplicationsController < ApplicationController
   end
 
   def create
-    application = Application.create(app_params)
-    redirect_to "/applications/#{application.id}"
+    application = Application.new(app_params)
+    if application.save
+      redirect_to "/applications/#{application.id}"
+    else
+      redirect_to "/applications/new"
+      flash[:alert] = "Error: #{error_message(application.errors)}"
+    end
   end
 
 private
