148f0a23dcd227e038c23e6a6a093e58dd640012
Shawn Lee <Shawnl93@hotmail.com>
2022-10-25 12:59:31 -0600

    US 4 completed

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'application show page' do
     @shelter_3 = Shelter.create!(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
     @pirate = @shelter_1.pets.create!(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
     @clawdia = @shelter_1.pets.create!(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
-    @lucille = @shelter_3.pets.create!(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
+    @lucille = @shelter_2.pets.create!(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
     @Shawn = Application.create!(name: "Shawn", street: "123 Turing st.", city: "Denver", state: "CO", zip_code: '80000', description: "pet lover", application_status: "In Progress")
     @Shawn.pets << @pirate
     @Shawn.pets << @clawdia
@@ -45,6 +45,17 @@ RSpec.describe 'application show page' do
       expect(page).to have_content(@clawdia.name)
       expect(page).to_not have_button("Adopt this Pet")
 
+    end
+
+    it "when no application submited, see section to add pet to application" do
+      expect(page).to have_content("Add A Pet to this Application")
+    end
+
+    it "can search for pet by name" do
+      fill_in 'Search', with: 'Lucille Bald'
+      click_on("Search")
+      expect(current_path).to eq("/applications/#{@Shawn.id}")
+      expect(page).to have_content(@lucille.name)
     end
 
   end
